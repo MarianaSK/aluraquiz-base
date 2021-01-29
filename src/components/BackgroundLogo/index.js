a6082aa81688ee3c03cd019eb9b8aab432e170c6
MarianaKuchida <mariana.kuchida@gmail.com>
2021-01-29 01:01:47 -0300

    minha aplicacao com base na imersao react aula 1

---
+++ src/components/BackgroundLogo/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const BackgroundLogo = styled.div`
+    height: 168px;
+    width: 100%;
+    background-color: #cecece7d;
+`;
+
+export default BackgroundLogo;
